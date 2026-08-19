28ed2f51dfb8f31b60a09624e4c7bb66e2460666
n0sfs <noslocire@gmail.com>
2026-08-18 22:49:11 -0400

    updated requirements and setup

--- src/setup_admin.py
+++ src/setup_admin.py
@@ -1,14 +1,13 @@
-import sqlite3, getpass, os
+import sqlite3, os
 from werkzeug.security import generate_password_hash
 def setup_admin():
-    conn = sqlite3.connect("/opt/micro-dfir/siem.db"); cursor = conn.cursor()
+    conn = sqlite3.connect("/opt/micro-dfir/siem.db")
+    cursor = conn.cursor()
     if not cursor.execute("SELECT * FROM users WHERE username='admin'").fetchone():
-        print("\n=== Security Setup ===")
-        while True:
-            pwd = getpass.getpass("Enter new password for 'admin' user: ")
-            if pwd == getpass.getpass("Confirm password: ") and len(pwd) > 4: break
-            print("Mismatch or too short. Try again.")
-        cursor.execute("INSERT INTO users (username, password_hash, role) VALUES ('admin', ?, 'admin')", (generate_password_hash(pwd),))
-        conn.commit(); print("[+] Admin user created.\n")
+        print("\n[*] Creating default Administrator account...")
+        pwd_hash = generate_password_hash("Admin123!")
+        cursor.execute("INSERT INTO users (username, password_hash, role) VALUES ('admin', ?, 'admin')", (pwd_hash,))
+        conn.commit()
+        print("[+] Admin user created. Username: admin | Password: Admin123!")
     conn.close()
 if __name__ == "__main__": setup_admin()
